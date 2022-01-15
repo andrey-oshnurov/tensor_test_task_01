b3e0cabedd3ffa237f346a78545f98c18c5f8518
andrey-oshnurov <oshnurov@gmail.com>
2022-01-15 13:25:14 +0500

    Add Reading Properties from file and Saving results to log

--- main.py
+++ main.py
@@ -1,53 +1,69 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import time
+
+log = ""
+
+# Read the file passed as parameter as a properties file.
+def load_properties(filepath, sep='=', comment_char='#'):
+    props = {}
+    with open(filepath, "rt", encoding="utf-8") as f:
+        for line in f:
+            l = line.strip()
+            if l and not l.startswith(comment_char):
+                key_value = l.split(sep)
+                key = key_value[0].strip()
+                value = sep.join(key_value[1:]).strip().strip('"')
+                props[key] = value
+    return props
 
 
+# Check if exist xpath
 def check_exists_by_xpath(xpath):
     return len(driver.find_elements(By.XPATH, xpath)) > 0
 
 
+# Check if exist suggestions
 def check_sugesstion_exists(suggestionsClassName):
     return len(driver.find_elements(By.CLASS_NAME, suggestionsClassName)) > 0
 
 
-driver = webdriver.Chrome(executable_path="C:\\app\\selenium\\webdriver\\chromedriver.exe")
-# driver = webdriver.Firefox(executable_path="C:\\app\\selenium\\webdriver\\geckodriver.exe")
-url = "https://yandex.ru/"
-xpath = ".//input[@id='text']"
-stringForSearch = "Тензор"
-suggestionsclassName = "mini-suggest__item"
+props = load_properties("file.cfg")
+ser = Service(props["SeleniumWebDriverPath"])
+opt = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=ser, options=opt)
 
 try:
-    driver.get(url=url)
+    driver.get(url=props["urlForTest"])
 
-    if check_exists_by_xpath(xpath):
+    if check_exists_by_xpath(props["searchInputXpath"]):
         driver.implicitly_wait(5)
-        print("Search Input was found")
-        inputElement = driver.find_element(By.XPATH, xpath)
-        inputElement.send_keys(stringForSearch)
-        if check_sugesstion_exists(suggestionsclassName):
-            print("Suggestions were found")
+        log += "Search Input was found\n"
+        inputElement = driver.find_element(By.XPATH, props["searchInputXpath"])
+        inputElement.send_keys(props["stringForSearch"])
+        if check_sugesstion_exists(props["suggestionsClassName"]):
+            log += "Suggestions were found\n"
         else:
-            print("Suggestions were not found")
+            log += "Suggestions were not found\n"
         inputElement.send_keys(Keys.ENTER)
 
-        #time.sleep(5)
-
-        searchResultsText = driver.find_elements(By.CSS_SELECTOR, ".organic__url")
+        searchResultsText = driver.find_elements(By.CSS_SELECTOR, props["searchResultLinksCssSelector"])
         i = 0
         for searchResultText in searchResultsText[:5]:
             s = searchResultText.get_attribute("href")
             if "tensor.ru" in s:
                 i += 1
         if i < 5:
-            print("All first 5 results do not contain a link to tensor.ru")
+            log += "All first 5 results do not contain a link to tensor.ru\n"
         elif i == 5:
-            print("All first 5 results contain a link to tensor.ru")
+            log += "All first 5 results contain a link to tensor.ru\n"
 
     else:
-        print("Error: Search Input was not found on page " + url)
+        log += "Error: Search Input was not found on page " + urlForTest + "\n"
+
+    f = open('result.log', 'w')
+    f.write(log)
 
 
 except Exception as exc:
